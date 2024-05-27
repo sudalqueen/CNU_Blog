084ea9159887029a67fdcd3de85b548bb85a92fc
김수정 <krystal@sk.com>
2024-05-27 10:28:46 +0900

    basename 추가

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools />
-      <Router>
+      <Router basename={'/CNU_Blog/'}>
         <Routes>
           <Route element={<Header />}>
             <Route element={<Layout />}>
