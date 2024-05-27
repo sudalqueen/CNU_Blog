bd401b4454739ce962f31ea8e01d006f3a17badd
김수정 <krystal@sk.com>
2024-05-27 17:39:39 +0900

    블로그 코드 작성

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -5,6 +5,8 @@ import { deletePostById, getPostById } from '../api';
 import { IPost } from '../api/types';
 import NotFound from '../components/NotFound';
 import Tag from '../components/Tag';
+import useGetPostById from '../queries/useGetPostById.ts';
+import useDeletePostById from '../queries/useDeletePostById.ts';
 
 const Title = styled.h1`
   font-size: 3rem;
@@ -62,24 +64,24 @@ const Text = styled.p`
 const Post = () => {
   const params = useParams();
   const { postId = '' } = params;
-  const [post, setPost] = useState<IPost | null>(null);
+  const { data: post, isError, isLoading } = useGetPostById(postId);
+  const { mutate: deletePost } = useDeletePostById();
 
-  const fetchPostById = async (id: string) => {
-    const { data } = await getPostById(id);
-    setPost(data);
+  const clickDeleteButton = () => {
+    const result = window.confirm('정말로 게시글을 삭제하시겠습니까?');
+    if (result) {
+      deletePost({ postId });
+    }
   };
 
-  useEffect(() => {
-    if (postId) {
-      fetchPostById(postId);
-    }
-  }, []);
+  if (isLoading) {
+    return <div>...불러오는 중...</div>;
+  }
 
-  if (!post) {
+  if (!post || isError) {
     return <NotFound />;
   }
 
-  // todo (4) post 컴포넌트 작성
   return (
     <div style={{ margin: '5.5rem auto', width: '700px' }}>
       <div>
@@ -89,9 +91,10 @@ const Post = () => {
             <div>n분전</div>
           </Info>
           <div>
-            {/*todo 수정/삭제 버튼 작성*/}
-            <TextButton>수정</TextButton>
-            <TextButton>삭제</TextButton>
+            <Link to="/write" state={{ postId }}>
+              <TextButton style={{ marginRight: 10 }}>수정</TextButton>
+            </Link>
+            <TextButton onClick={clickDeleteButton}>삭제</TextButton>
           </div>
         </Toolbar>
         {post?.tag && (
